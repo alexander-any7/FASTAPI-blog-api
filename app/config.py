ba170d4460dbaa67948b5d6efa1fe1b80c3cb8f7
Anyaegbunam-Alexander <alexanderking.aa@gmail.com>
2022-12-02 10:25:31 +0100

    'testing

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     database_username: str
     secret_key: str
     algorithm: str
-    access_token_expires_minutes: int
+    access_token_expires_minutes: str
 
     class Config:
         env_file = '.env'
